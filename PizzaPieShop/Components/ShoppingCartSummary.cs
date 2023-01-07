6230bd8aa143317b60a1463978c02318c9b6ccec
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-01-07 21:58:30 +0200

    add shopping card as viewcompont and add its controller

---
+++ PizzaPieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,23 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using PizzaPieShop.ViewModels;
+
+namespace PizzaPieShop.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var items = new List<ShoppingCartItem>() { new ShoppingCartItem(), new ShoppingCartItem() };
+            //var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
+            return View(shoppingCartViewModel);
+        }
+    }
+}

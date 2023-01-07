21e50509288ee417c67e4c67c1651d31a5bb4b20
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-01-07 19:44:06 +0200

    add home page

---
+++ PizzaPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PizzaPieShop.Models;
+
+namespace PizzaPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; } 
+        public HomeViewModel(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek = piesOfTheWeek;
+        }
+    }
+}

6aba00a3eb4819cd257aeaa20124205b4af12ac8
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-01-08 14:03:32 +0200

    test controller

---
+++ PizzaPieShop Tests/Controllers/PieControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPieShop.Controllers;
+using PizzaPieShop.ViewModels;
+using PizzaPieShop_Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PizzaPieShop_Tests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnsAllPies()
+        {
+            //arrange
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            //act
+            var result = pieController.List("");
+
+            //assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}

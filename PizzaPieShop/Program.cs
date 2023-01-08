e27ae564bf9f29f8d32b56030609a34c47f5e248
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-01-09 00:34:10 +0200

    register Blazor page in  program1

--- PizzaPieShop/Program.cs
+++ PizzaPieShop/Program.cs
@@ -47,6 +47,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/app/{*catchall}", "/App/Index");
 // to seed data 
 DbInitializer.Seed(app);
 

e9a1ccb1355f0c060d23b4504508b5c723a67323
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-01-09 00:45:58 +0200

    convert razor pages to blazor pages

---
+++ PizzaPieShop/Pages/App/PieCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using PizzaPieShop.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace PizzaPieShop.Pages.App
+{
+    public partial class PieCard
+    {
+        [Parameter]
+        public Pie? Pie { get; set; }
+    }
+}
---
+++ PizzaPieShop/Pages/App/SearchBlazor.razor.cs
@@ -0,0 +1,25 @@
+﻿using PizzaPieShop.Models;
+using Microsoft.AspNetCore.Components;
+using PizzaPieShop.IRepositories;
+
+namespace PizzaPieShop.Pages.App
+{
+    public partial class SearchBlazor
+    {
+        public string SearchText = "";
+        public List<Pie> FilteredPies { get; set; } = new List<Pie>();
+
+        [Inject]
+        public IPieRepository? PieRepository { get; set; }
+
+        private void Search()
+        {
+            FilteredPies.Clear();
+            if (PieRepository is not null)
+            {
+                if (SearchText.Length >= 3)
+                    FilteredPies = PieRepository.SearchPies(SearchText).ToList();
+            }
+        }
+    }
+}
